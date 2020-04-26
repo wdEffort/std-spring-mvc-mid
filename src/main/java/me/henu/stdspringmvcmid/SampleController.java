5fa49294ce12f17810251fa5040928f78d7a6ecb
wdEffort <yoman3011@gmail.com>
2020-04-26 12:18:34 +0900

    @PathVariable Optional을 사용한 부가적인 처리하기

--- src/main/java/me/henu/stdspringmvcmid/SampleController.java
+++ src/main/java/me/henu/stdspringmvcmid/SampleController.java
@@ -5,11 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Optional;
+
 @Controller
 public class SampleController {
 
     /**
      * URI 패턴의 일부를 핸들러 메소드 아규먼트로 받는 방법
+     * Java 8 이후부터 제공하는 Optional을 사용하여 부가적인 처리를 할 수 있음
      *
      * @return
      * @PathVariable 어노테이션을 사용하면 URI Path를 값으로 받아올 수 있음(값이 반드시 있어야 함, 타입 변환과 Optional을 지원함)
@@ -17,9 +20,12 @@ public class SampleController {
      */
     @GetMapping("/events/{id}")
     @ResponseBody
-    public Event getEvent(@PathVariable Integer id) {
+    public Event getEvent(@PathVariable Optional<Integer> id) {
         Event event = new Event();
-        event.setId(id);
+
+        if (!id.isEmpty()) {
+            event.setId(id.get());
+        }
 
         return event;
     }
