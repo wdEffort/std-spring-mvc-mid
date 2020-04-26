80bbe0b759688ef44257c61e9967581fb3fa5b4b
wdEffort <yoman3011@gmail.com>
2020-04-26 23:01:02 +0900

    모델 정보를 HTTP Session에 저장해주는 어노테이션 사용하기(@SessionAttributes, Multi Form Submit 구현)

--- src/main/java/me/henu/stdspringmvcmid/SampleController.java
+++ src/main/java/me/henu/stdspringmvcmid/SampleController.java
@@ -5,56 +5,91 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
-import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+/**
+ * @SessionAttributes 어노테이션을 사용하면
+ * 설정한 이름(여기서는 "event")에 해당하는 Model 객체를 자동으로 Session에 넣어줌.
+ * 여러 화면(또는 요청)에서 사용해야 하는 객체를 공유할 때 사용.
+ *
+ * [특징]
+ * 1. @ModelAttribute 어노테이션을 사용하여 세션에 있는 데이터를 바인딩 시킬 수 있음
+ * 2. SessionStatus 객체를 이용하여 세션 처리를 완료시킬 수 있음(ex. Form 데이터 처리가 끝나고 세션을 비울 때)
+ * - 보통 @SessionAttributes와 함께 쓰임
+ */
+@SessionAttributes("event")
 public class SampleController {
 
     /**
-     * 이벤트를 입력하는 페이지로 이동
-     * HttpSession 객체를 이용하여 Session에 값을 넣고, 뺴는 것이 가능함.
+     * 이벤트를 이름을 입력하는 페이지로 이동
      *
      * @param model
      * @return
      */
-    @GetMapping("/events/form")
-    public String eventsForm(Model model, HttpSession httpSession) {
-        Event newEvent = new Event();
-        newEvent.setLimit(50);
-
-        model.addAttribute("event", newEvent); // Form backing object
-
-        httpSession.setAttribute("event", newEvent);
-
-        return "events/form";
+    @GetMapping("/events/form/name")
+    public String eventsFormName(Model model) {
+        model.addAttribute("event", new Event()); // @SessionAttributes에서 설정한 이름과 동일한 Model 객체이므로 Session에 저장됨.
+        return "events/form-name";
     }
 
     /**
-     * 새로운 이벤트 생성
+     * 이벤트 이름을 처리
+     * 데이터를 @ModelAttribute 어노테이션을 사용하여 복합 객체 타입으로 바인딩 할 때
+     * Session에 담긴 해당 객체의 정보가 변경된 다음에 바인딩이 됨.
      *
      * @param event
      * @param bindingResult
+     * @param sessionStatus
      * @return
      */
-    @PostMapping("/events")
-    public String createEvent(
+    @PostMapping("/events/form/name")
+    public String eventFormNameSubmit(
             @Validated @ModelAttribute Event event,
             BindingResult bindingResult,
-            Model model) {
-        // BindingResult 타입의 변수에 에러가 있으면 if문 동작
+            SessionStatus sessionStatus) {
         if (bindingResult.hasErrors()) {
-            return "events/form"; // View에서 바인딩 에러를 보여줄 수 있음(Thymeleaf, JSP 마다 사용법이 있으니 참고...)
+            return "events/form-name";
         }
 
-        List<Event> events = new ArrayList<>();
-        events.add(event);
+        return "redirect:/events/form/limit";
+    }
 
-        model.addAttribute("eventList", events);
+    /**
+     * 이벤트 참여자수를 입력하는 페이지로 이동
+     *
+     * @param event
+     * @param model
+     * @return
+     */
+    @GetMapping("/events/form/limit")
+    public String eventsFormLimit(@ModelAttribute Event event, Model model) {
+        model.addAttribute("event", event);
+        return "events/form-limit";
+    }
 
-        // POST > Redirect > GET 패턴 적용
+    /**
+     * 이벤트 참여자수를 처리
+     *
+     * @param event
+     * @param bindingResult
+     * @param sessionStatus
+     * @return
+     */
+    @PostMapping("/events/form/limit")
+    public String eventFormLimitSubmit(
+            @Validated @ModelAttribute Event event,
+            BindingResult bindingResult,
+            SessionStatus sessionStatus) {
+        if (bindingResult.hasErrors()) {
+            return "events/form-limit";
+        }
+
+        sessionStatus.setComplete(); // @SessionAttributes로 지정한 객체를 Session에서 지움
+
         return "redirect:/events/list";
     }
 
