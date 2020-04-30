7b79c5a1c670982de98189a33051eda1816932a0
wdEffort <yoman3011@gmail.com>
2020-04-30 10:36:20 +0900

    File 다운로드(ResourceLoader, ResponseEntity, Tika 라이브러리 사용)

--- src/main/java/me/henu/stdspringmvcmid/FileController.java
+++ src/main/java/me/henu/stdspringmvcmid/FileController.java
@@ -1,15 +1,27 @@
 package me.henu.stdspringmvcmid;
 
+import org.apache.tika.Tika;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
 @Controller
 public class FileController {
+
+    // 파일 리소스를 읽어오기 위한 의존성 주입
+    @Autowired
+    ResourceLoader resourceLoader;
 
     /**
      * File 업로드 양식 화면 요청
@@ -55,4 +67,35 @@ public class FileController {
 
     }
 
+    /**
+     * 파일 다운로드
+     * 1. 파일 리소스를 읽어오는 방법
+     * - Spring에서 제공하는 ResourceLoader를 주입받아 사용하면 됨.
+     * <p>
+     * ResponseEntity<T>
+     * 1. 응답 상태코드, 응답 헤더, 응답 본문을 설정할 수 있음.
+     * - 응답 본문의 타입을 제네릭 타입으로 정의 할 수 있음.
+     *
+     * @param filename
+     * @return
+     * @throws Exception
+     */
+    @GetMapping("/file/{filename}")
+    // 리턴 타입이 ResponseEntity인 경우에는 응답 본문을 작성한 것이기 때문에 @ResponseBody를 사용하지 않아도 됨.
+    // @ResponseBody
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) throws Exception {
+        Resource resource = resourceLoader.getResource("classpath:" + filename);
+        File file = resource.getFile();
+
+        // 미디어 타입을 알아내기 위해 Tika 사용
+        Tika tika = new Tika();
+        String mediaType = tika.detect(file);
+
+        // 파일 다운로드 응답 헤더(문자열 타입)에 설정할 내용을 지정함.
+        return ResponseEntity.ok() // 응답 상태 코드 설정
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"") // 사용자가 해당 파일을 받을 때 사용할 파일 이름 설정
+                .header(HttpHeaders.CONTENT_TYPE, mediaType) // 어떤 타입의 파일인지 설정
+                .header(HttpHeaders.CONTENT_LENGTH, file.length() + "") // 파일 크기 설정(Header는 모두 문자열 타입이어야 하므로 형변환이 필요함.)
+                .body(resource); // 응답 본문에 리소스를 넣어줌으로써 다운로드가 가능해짐
+    }
 }
