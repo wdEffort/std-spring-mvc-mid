7a1b69709ae19d33922dce968e9939d06aa81710
wdEffort <yoman3011@gmail.com>
2020-04-30 11:18:50 +0900

    요청 헤더, 요청 본문 모두 접근할 수 있는 HttpEntity 사용하기

--- src/main/java/me/henu/stdspringmvcmid/EventApi.java
+++ src/main/java/me/henu/stdspringmvcmid/EventApi.java
@@ -1,7 +1,8 @@
 package me.henu.stdspringmvcmid;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,23 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventApi {
 
     /**
-     * 요청 본문(Body)에 들어오는 데이터 @RequestBody 어노테이션을 사용하여
-     * HandlerAdopter가 메소드 아큐먼트를 Resolving할 때
-     * HandlerAdopter에 등록되어 있는 여러 HttpMessageConverter 중에
-     * 현재 요청에 들어 있는 본문을 변환 할 수 있는 Converter를 선택을 해서 변환한 객체로 받아올 수 있음.
-     * - 가령 요청 본문이 JSON 타입이고, 요청 Header의 Content-Type이 JSON인 경우 JSON을 변환할 수 있는 HttpMessageConverter를 사용하게 됨.
-     * <p>
-     * [단점]
-     * 1. 요청 헤더 정보에는 접근할 수 없고, 오로지 요청 본문 정보에만 접근이 가능함.
+     * @ResponseBody와 비슷하지만, 요청 헤더 정보까지 가져오기 위해서는 HttpEntity<T>를 사용해야 함.
+     * - HttpMessageConverter를 이용하여 요청 본문 데이터를 객체로 변환할 수 있음.
+     * - 요청 본문 데이터를 변환한 객체의 타입을 제네릭 타입으로 설정.
      *
-     * @param event
+     * @param request
      * @return
      */
     @PostMapping
-    public Event createEvent(@RequestBody Event event) {
+    public Event createEvent(HttpEntity<Event> request) {
         // DB가 있다면 이벤트를 저장할 수 있음.
 
-        return event;
+        // 요청 헤더 정보에 접근해서 Content-Type 추출.
+        MediaType contentType = request.getHeaders().getContentType();
+        System.out.println(contentType);
+
+        return request.getBody(); // 제네릭 타입에 해당하는 본문을 반환함.
     }
 
 
