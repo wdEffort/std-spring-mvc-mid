bf5df735ac86e50b8f57c6099d90b30689a9af2d
wdEffort <yoman3011@gmail.com>
2020-05-01 10:46:04 +0900

    REST API에서 예외 처리 핸들러 사용하기

--- src/main/java/me/henu/stdspringmvcmid/EventApi.java
+++ src/main/java/me/henu/stdspringmvcmid/EventApi.java
@@ -3,6 +3,7 @@ package me.henu.stdspringmvcmid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,13 +14,26 @@ import javax.validation.Valid;
 public class EventApi {
 
     /**
+     * REST API의 경우 응답 본문에 에러 정보를 담아주고, 상태 코드를 설정하려면 ResponseEntity를 주로 사용함.
+     * - REST API에서는 메시지 본문에 왜 에러가 발생했는지 에러 정보를 전달해줘야 하기 떄문
+     *
+     * @param exception
+     * @param model
+     * @return
+     */
+    @ExceptionHandler
+    public ResponseEntity eventErrorHandler(RuntimeException exception, Model model) {
+        return ResponseEntity.badRequest().body("Can't create event as ...");
+    }
+
+    /**
      * 데이터를 응답 본문 메시지로 보내기
-     * @ResponseBody를 사용하지 않고, 메소드 리턴 타입으로 ResponseEntity<T>를 사용.
-     * - 응답 본문에 전달할 메시지 타입을 제네릭 타입으로 설정함.
-     * - 좀 더 세밀한 응답 처리를 해야하는 경우 사용하면 됨.
      *
      * @param event
      * @return
+     * @ResponseBody를 사용하지 않고, 메소드 리턴 타입으로 ResponseEntity<T>를 사용.
+     * - 응답 본문에 전달할 메시지 타입을 제네릭 타입으로 설정함.
+     * - 좀 더 세밀한 응답 처리를 해야하는 경우 사용하면 됨.
      */
     @PostMapping
     public ResponseEntity<Event> createEvent(@RequestBody @Valid Event event, BindingResult bindingResult) {
